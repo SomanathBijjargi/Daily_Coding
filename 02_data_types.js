d9ddcd9615fa19f01372932a6d3832ca358ced25
SomanathBijjargi <152138667+SomanathBijjargi@users.noreply.github.com>
2025-03-08 08:50:12 +0000

    Data types

---
+++ 02_data_types.js
@@ -0,0 +1,20 @@
+// null is not equals to undefined ..
+
+let a ;
+console.log(typeof a);
+
+let  b=null;
+console.log(typeof b);
+
+let  c="apple";
+console.log(typeof c);
+
+let  d=2;
+console.log(typeof d);
+
+let  e=true;
+console.log(typeof e);
+
+let  f=undefined;
+console.log(typeof f);
+
